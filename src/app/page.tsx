c9eb116c367314ffa63e8029fb4cb4a5b9327f55
Kodai <hanadaneko0618@gmail.com>
2024-03-08 23:05:32 +0900

    大樹さん作成の文書を定数として格納

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { OriginalTheme } from "@/style/theme";
-import Header from "../components/header/header";
+import Header from "@/components/header/header";
 import { Stack, useMediaQuery } from "@mui/material";
+import Image from "next/image";
+import BusinessIntroduction from "@/components/businessIntroduction/businessIntroduction";
+// import preliminaryPhoto from "@/public/preliminaryPhoto.png";
 
 export default function Home(){
   const isPc = useMediaQuery("(min-width: 600px)")
@@ -9,6 +12,15 @@ export default function Home(){
     <OriginalTheme>
       <Stack>
         <Header isPc={isPc}/>
+          {/* TODO: 画像を差し替える。その際に見せ方は調整する */}
+        <Image
+        src="/preliminaryPhoto.png" 
+        alt="preliminaryPhoto" 
+        width={1280} height={852} objectFit="contain" 
+        style={{ width: '100%', height: 'auto' }} 
+        />
+
+        <BusinessIntroduction/>
       </Stack>
     </OriginalTheme>
   )
---
+++ src/components/businessIntroduction/businessIntroduction.tsx
@@ -0,0 +1,7 @@
+export default function BusinessIntroduction() {
+  return (
+    <div>
+      <h1>BusinessIntroduction</h1>
+    </div>
+  );
+}
